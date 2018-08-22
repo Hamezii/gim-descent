9e44dd35d3a905bd0544008ead60da39abea6492
Hamezii <36954453+Hamezii@users.noreply.github.com>
2018-08-22 10:53:51 +0100

    Use lru_cache for caching

--- renderer.py
+++ renderer.py
@@ -1,12 +1,22 @@
 '''
 Contains the Renderer of Gim Descent.
 '''
+from functools import lru_cache
 
 import pygame
 
 import constants
 
 # CONSTANTS
+
+@lru_cache(maxsize=None)
+def _import_image(name):
+    try:
+        image = pygame.image.load(constants.IMAGES+name+".png").convert_alpha()
+    except pygame.error:
+        image = pygame.image.load(constants.DEFAULT_IMAGES+name+".png").convert_alpha()
+    return image
+
 
 class Renderer:
     """Rendering wrapper which stores cached surfaces."""
@@ -16,16 +26,15 @@ class Renderer:
 
     def __init__(self):
         self.camera = None
-        self._imported_images = {}
-        self.images = {}
         self.total_images = 0
 
         self.t_elapsed = 0
 
+    @lru_cache(maxsize=None)
     def get_image(self, **args):
-        """Get an image surface from the cache. If it does not exist, the image is created.
+        """Get an image. Optional modifier parameters like scale and color can be used.
 
-        Optional modifier parameters like scale and color can be used.
+        Function is cached with lru_cache.
         """
         if "scale" not in args:
             args["scale"] = 1
@@ -36,35 +45,19 @@ class Renderer:
             else:
                 del args["blinking"]
 
-        key = str(args)
+        self.total_images += 1
+        image = _import_image(args["name"]).copy()
+        if "scale" in args:
+            image = pygame.transform.scale(image, (int(image.get_width()*args["scale"]), int(image.get_height()*args["scale"])))
 
-        if key not in self.images:
-            self.total_images += 1
-            self.images[key] = self._import_image(args["name"])
-            if "scale" in args:
-                self.images[key] = pygame.transform.scale(self.images[key], (int(
-                    self.images[key].get_width()*args["scale"]), int(self.images[key].get_height()*args["scale"])))
+        if "color" in args:
+            image.fill(args["color"][0:3], special_flags=args["color"][3])
 
-            if "color" in args:
-                self.images[key].fill(args["color"][0:3], special_flags=args["color"][3])
+        if "blinking" in args:
+            if args["blinking"]:
+                image.fill((50, 50, 50), special_flags=pygame.BLEND_ADD)
 
-            if "blinking" in args:
-                if args["blinking"]:
-                    self.images[key].fill((50, 50, 50), special_flags=pygame.BLEND_ADD)
-
-
-
-        return self.images[key]
-
-    def _import_image(self, name):
-        if not name in self._imported_images:
-            try:
-                image = pygame.image.load(constants.IMAGES+name+".png").convert_alpha()
-            except pygame.error:
-                image = pygame.image.load(constants.DEFAULT_IMAGES+name+".png").convert_alpha()
-            self._imported_images[name] = image
-
-        return self._imported_images[name].copy()
+        return image
 
     def draw_text(self, surface, color, pos, text, size, centered=False):
         """Draw text to a surface.
@@ -100,5 +93,4 @@ class Renderer:
 
     def draw_centered_image(self, surface, image, centerpos):
         """Blit an image to a surface, centering it at centerpos."""
-        surface.blit(
-            image, (centerpos[0] - image.get_width()//2, centerpos[1] - image.get_height()//2))
+        surface.blit(image, (centerpos[0] - image.get_width()//2, centerpos[1] - image.get_height()//2))
