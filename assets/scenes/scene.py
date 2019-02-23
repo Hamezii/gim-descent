a2e6e9aa15777840c922d57587638bdc5e8423b1
Hamezii <36954453+Hamezii@users.noreply.github.com>
2019-02-23 12:01:44 +0000

    Make settings menu more functional Remove has_focus_scene from scene.py since it wasn't being used

--- assets/scenes/scene.py
+++ assets/scenes/scene.py
@@ -23,10 +23,6 @@ class Scene:
         """Remove this scene from the scene tree."""
         self.game.remove_scene(self)
 
-    def has_focus_scene(self):
-        """Return True if this scene or any of its children are the focus scene."""
-        return any((self is self.game.focus_scene, *(child.has_focus_scene() for child in self.children)))
-
     def call_recursively(self, event_name, *args, **kwargs):
         """Call a function on this scene given its name and parameters.
 
--- assets/scenes/settings.py
+++ assets/scenes/settings.py
@@ -32,22 +32,50 @@ class Settings(Scene):
                 size=constants.MENU_SCALE*10,
                 color=constants.DARK_GREEN,
                 centered=True
+                ),
+            wgt.TextLines(
+                renderer=self.game.renderer,
+                text=["This menu is a work in progress.", "Graphics settings will take effect on a game restart."],
+                offset=(self.box_rect.centerx, self.box_rect.bottom + constants.MENU_SCALE * 15),
+                size=constants.MENU_SCALE*5,
+                color=constants.LIGHT_GRAY,
+                centered=True
                 )
         ]
+        self.options = []
         toggle_pos = (self.box_rect.left + 10 * constants.MENU_SCALE, self.box_rect.top + 40 * constants.MENU_SCALE)
-        self.toggle = self.add_child_scene(Toggle, "Fullscreen", toggle_pos, state=self.config.fullscreen_mode)
-        self.toggle.connect_signal("changed_state", lambda x: self.set_setting("fullscreen_mode", x))
+        toggle = self.add_child_scene(Toggle, "Fullscreen", toggle_pos, state=self.config.fullscreen_mode)
+        toggle.connect_signal("changed_state", lambda x: self.set_setting("fullscreen_mode", x))
+        self.options.append(toggle)
+        self.option_index = None
 
     def set_setting(self, setting, value):
         """Set a setting to the specified value."""
         setattr(self.config, setting, value)
 
     def handle_input(self, keypress):
-        self.game.set_focus(self.toggle) # testing
         if keypress.has_action(key_input.Action.BACK):
             self.config.save_to_file()
             self.remove_scene()
+        if keypress.has_action(key_input.Action.UP):
+            self._change_option(max(self.option_index-1, 0))
+        if keypress.has_action(key_input.Action.DOWN):
+            self._change_option(min(self.option_index+1, len(self.options)-1))
+
         return True
+
+    def update(self, delta):
+        if self.option_index is None: # Done like this since if Settings sets this focus in init, Settings focusing afterwards will override it.
+            self.option_index = 0
+            self.game.set_focus(self.options[0])
+
+    def _change_option(self, index):
+        """Change to a different option, unfocusing the last one and focusing the new one."""
+        if self.option_index == index:
+            return
+        self.game.remove_focus(self.options[self.option_index])
+        self.option_index = index
+        self.game.set_focus(self.options[self.option_index])
 
     def draw(self, screen):
         pygame.draw.rect(screen, constants.DARK_GRAY, self.box_rect.inflate(self.border_size, self.border_size))
